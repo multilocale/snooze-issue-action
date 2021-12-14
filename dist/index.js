13c9b3dd02baa829bc9535cebd9795e9de52421d
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2021-12-14 12:48:53 +0100

    feat: add eslint

--- dist/index.js
+++ dist/index.js
@@ -8446,36 +8446,33 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 async function run() {
-
   try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
     // console.log(`The event payload: ${payload}`);
 
-    
     const githubToken = core.getInput('githubToken');
     const octokit = github.getOctokit(githubToken);
 
-    console.log({octokit})
+    console.log({ octokit });
 
-    console.log('octokit.issues', octokit.issues)
+    console.log('octokit.issues', octokit.issues);
 
-    console.log('octokit?.issues?.update', octokit?.issues?.update)
+    console.log('octokit.issues.update', octokit.issues.update);
 
-    console.log('github.context.issue', github?.context?.issue)
+    console.log('github.context.issue', github.context.issue);
 
-    const result = await octokit.issues.update(context.issue({
+    const result = await octokit.issues.update(github.context.issue({
       state: 'closed',
-    }))
+    }));
 
-    console.log({result})
-
+    console.log({ result });
   } catch (error) {
     core.setFailed(error.message);
   }
-
 }
 
-run()
+run();
+
 })();
 
 module.exports = __webpack_exports__;
--- index.js
+++ index.js
@@ -2,33 +2,29 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-
   try {
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // const payload = JSON.stringify(github.context.payload, undefined, 2);
     // console.log(`The event payload: ${payload}`);
 
-    
     const githubToken = core.getInput('githubToken');
     const octokit = github.getOctokit(githubToken);
 
-    console.log({octokit})
+    console.log({ octokit });
 
-    console.log('octokit.issues', octokit.issues)
+    console.log('octokit.issues', octokit.issues);
 
-    console.log('octokit?.issues?.update', octokit?.issues?.update)
+    console.log('octokit.issues.update', octokit.issues.update);
 
-    console.log('github.context.issue', github?.context?.issue)
+    console.log('github.context.issue', github.context.issue);
 
-    const result = await octokit.issues.update(context.issue({
+    const result = await octokit.issues.update(github.context.issue({
       state: 'closed',
-    }))
+    }));
 
-    console.log({result})
-
+    console.log({ result });
   } catch (error) {
     core.setFailed(error.message);
   }
-
 }
 
-run()
+run();
