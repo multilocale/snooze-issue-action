e132c6c14bcdcdea8a38be40ee85fbfd28d924d7
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2021-12-14 11:39:47 +0100

    feat: check octokit is readable

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
 
   const octokit = github.getOctokit(githubToken);
 
-  const { data: comments } = await octokit.issues.listComments({
-    ...repo,
-    issue_number: pullRequestNumber,
-  });
+  console.log({octokit})
 
-  console.log({comments})
+  console.log({octokit.issues})
+
+  // const { data: comments } = await octokit.issues.listComments({
+  //   ...repo,
+  //   issue_number: pullRequestNumber,
+  // });
+
+  // console.log(`t: ${comments}`)
 
 } catch (error) {
   core.setFailed(error.message);
