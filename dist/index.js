cc20f4369b6bae73d733b922f5338887ec090f23
Giorgio Zamparelli <giorgio.zamparelli@gmail.com>
2021-12-18 18:35:20 +0100

    feat: add data in comment

--- dist/index.js
+++ dist/index.js
@@ -8459,39 +8459,65 @@ async function run() {
     const repo = github.context.repo.repo
     const owner = github.context.repo.owner
     const issue_number = github.context.payload.issue.number
-    console.log('github.context.payload', github.context.payload)
+
+    const commentBody = github.context.payload.comment.body
 
     console.log({ repo })
     console.log({ owner })
     console.log({ issue_number })
+    console.log('github.context.payload')
 
-    const commentCreated = await octokit.rest.issues
-      .createComment({
+    if (commentBody.startsWith('/snooze')) {
+      let days = 7
+
+      let daysToParse = commentBody
+        .replace('/snooze', '')
+        .replace(' ', '')
+        .replace('day', '')
+        .replace('days', '')
+
+      if (daysToParse) {
+        try {
+          days = parseInt(daysToParse, 10)
+        } catch (error) {
+          console.log('error while parsing snooze time: ' + error)
+        }
+      }
+
+      const snoozeData = new Date(
+        Date.now() + days * 24 * 60 * 60 * 1000,
+      ).toISOString()
+
+      const snoozeComment =
+        `This issue has been snoozed for ${days} days` +
+        '\n' +
+        `<!-- snooze = ${JSON.stringify(snoozeData)} -->`
+
+      await octokit.rest.issues.createComment({
         repo,
         owner,
         issue_number,
-        body: 'body of the comment',
-      })
-      .then(error => {
-        console.log('error for octokit.rest.issues.createComment')
-        console.log('error')
+        body: snoozeComment,
       })
 
-    console.log({ commentCreated })
+      console.log({ commentCreated })
 
-    const issueClosed = await octokit.rest.issues
-      .update({
+      const labels = github.context.payload.issue.labels
+
+      if (!labels.includes('snoozed')) {
+        labels = labels.concat('snoozed')
+      }
+
+      await octokit.rest.issues.update({
         repo,
         owner,
         issue_number,
         state: 'closed',
-      })
-      .then(error => {
-        console.log('error for octokit.rest.issues.update')
-        console.log('error')
+        labels,
       })
 
-    console.log({ issueClosed })
+      console.log({ issueClosed })
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
